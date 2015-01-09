617cc102318736431773240ed2549bac44238d87
Luiz Branco <me@luizbranco.com>
2015-01-09 02:40:35 -0500

    Add basic player movement

---
+++ Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	
+	public float movementSpeed = 3.0f;
+
+	private void Update () {
+		Vector3 scale = transform.localScale;
+		Vector3 pos = transform.position;
+
+		float speed = Time.deltaTime * movementSpeed;
+		
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			scale.x = -1;
+			pos.x -= speed;
+			transform.localScale = scale;
+			transform.position = pos;
+		} 
+		
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			scale.x = 1;
+			pos.x += speed;
+			transform.localScale = scale;
+			transform.position = pos;
+		} 
+		
+		if (Input.GetKeyDown(KeyCode.UpArrow)) {
+			//TODO
+		} 
+		
+		if (Input.GetKeyDown(KeyCode.DownArrow)) {
+			//TODO
+		}
+	}
+
+}
