47355da27cdab67d110891f5c4c117987b9ff019
Luiz Branco <me@luizbranco.com>
2015-01-09 05:53:58 -0500

    Fix lvls collision boxes

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -5,13 +5,14 @@ public class PlayerMovement : MonoBehaviour {
 	
 	public float speed = 3.0f;
 	private bool isGrounded = true;
+	private bool isClimbing = false;
 
 	private void Update () {
 		
 		if (Input.GetKey(KeyCode.LeftArrow)) {
 			transform.Translate(Vector3.right * speed * Time.deltaTime); 
 			transform.eulerAngles = new Vector2(0, 180);
-		} 
+		}
 		
 		if (Input.GetKey(KeyCode.RightArrow)) {
 			transform.Translate(Vector3.right * speed * Time.deltaTime); 
@@ -26,7 +27,7 @@ public class PlayerMovement : MonoBehaviour {
 			//TODO
 		}
 
-		if (Input.GetKeyDown (KeyCode.Space) && isGrounded) {
+		if (Input.GetKeyDown(KeyCode.Space) && isGrounded && !isClimbing) {
 			rigidbody2D.AddForce(transform.up * 200f);
 		}
 	}
