64edcd784a927179cef19d817ab4622f969229ac
Luiz Branco <me@luizbranco.com>
2015-01-09 06:53:08 -0500

    Initial instructions scene

--- Assets/scripts/ui/MenuOptions.cs
+++ Assets/scripts/ui/MenuOptions.cs
@@ -4,7 +4,23 @@ using System.Collections;
 public class MenuOptions : MonoBehaviour {
 
 	public void StartGame () {
-		Application.LoadLevel(2);
+		Application.LoadLevel("Level01");
+	}
+	
+	public void LoadMenu () {
+		Application.LoadLevel("Menu");
+	}
+
+	public void LoadInstructions () {
+		Application.LoadLevel("Instructions");
+	}
+
+	public void LoadCredits () {
+		Application.LoadLevel("Credits");
+	}
+
+	public void QuitGame () {
+		Application.Quit();
 	}
 
 }
