eb1aa61bfef98d34067c2d910a2f22fa9914dd82
Luiz Branco <me@luizbranco.com>
2015-01-09 23:13:58 -0500

    Add jump animation

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -3,18 +3,33 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 	
-	public float maxSpeed = 5f;
+	public float hSpeed = 5f;
+	public float vForce = 400f;
+
 	private bool isFacingRight = true;
-	private bool isGrounded = true;
+	private bool isGrounded = false;
 	private bool isClimbing = false;
+
+	public Transform groundChecker;
+	public float groundedRadius = 0.2f;
+	public LayerMask groundLayer;
 
 	Animator anim;
 
-	private void Start () {
+	private void Awake () {
 		anim = GetComponent<Animator>();
 	}
 
 	private void Update () {
+
+		isGrounded = Physics2D.OverlapCircle(groundChecker.position, groundedRadius, groundLayer);
+		anim.SetBool("Grounded", isGrounded);
+
+		if (Input.GetKeyDown(KeyCode.Space) && isGrounded) {
+			anim.SetBool("Grounded", false);
+			anim.SetFloat("VSpeed", rigidbody2D.velocity.y);
+			rigidbody2D.AddForce(new Vector2(0, vForce));
+		}
 
 		if (Input.GetKeyDown(KeyCode.Escape)) {
 			Application.LoadLevel("Menu");
@@ -23,18 +38,21 @@ public class PlayerMovement : MonoBehaviour {
 	}
 
 	private void FixedUpdate () {
-		//TODO check for grounded
-		float move = Input.GetAxis("Horizontal");
 
-		rigidbody2D.velocity = new Vector2(move * maxSpeed, rigidbody2D.velocity.y);
-
-		anim.SetFloat("Speed", Mathf.Abs(move));
-
-		if (move > 0 && !isFacingRight) {
-			Flip ();
-		} else if (move < 0 && isFacingRight) {
-			Flip ();
+		if (isGrounded) {
+			float move = Input.GetAxis("Horizontal");
+			
+			rigidbody2D.velocity = new Vector2(move * hSpeed, rigidbody2D.velocity.y);
+			
+			anim.SetFloat("HSpeed", Mathf.Abs(move));
+			
+			if (move > 0 && !isFacingRight) {
+				Flip ();
+			} else if (move < 0 && isFacingRight) {
+				Flip ();
+			}
 		}
+
 	}
 
 	private void Flip () {
