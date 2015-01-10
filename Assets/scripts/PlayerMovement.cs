43962856515210ccf7c3108fc9256489999047df
Luiz Branco <me@luizbranco.com>
2015-01-09 21:53:12 -0500

    Improve walking code

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -3,41 +3,45 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 	
-	public float speed = 3.0f;
+	public float maxSpeed = 5f;
+	private bool isFacingRight = true;
 	private bool isGrounded = true;
 	private bool isClimbing = false;
 
-	private void Update () {
-		
-		if (Input.GetKey(KeyCode.LeftArrow)) {
-			transform.Translate(Vector3.right * speed * Time.deltaTime); 
-			transform.eulerAngles = new Vector2(0, 180);
-		}
-		
-		if (Input.GetKey(KeyCode.RightArrow)) {
-			transform.Translate(Vector3.right * speed * Time.deltaTime); 
-			transform.eulerAngles = new Vector2(0, 0);
-		} 
-		
-		if (Input.GetKeyDown(KeyCode.UpArrow)) {
-			//TODO
-		} 
-		
-		if (Input.GetKeyDown(KeyCode.DownArrow)) {
-			//TODO
-		}
+	Animator anim;
 
-		if (Input.GetKeyDown(KeyCode.Space) && isGrounded && !isClimbing) {
-			rigidbody2D.AddForce(transform.up * 200f);
-		}
+	private void Start () {
+		anim = GetComponent<Animator>();
+	}
+
+	private void Update () {
 
 		if (Input.GetKeyDown(KeyCode.Escape)) {
 			Application.LoadLevel("Menu");
 		}
+
 	}
 
 	private void FixedUpdate () {
 		//TODO check for grounded
+		float move = Input.GetAxis("Horizontal");
+
+		rigidbody2D.velocity = new Vector2(move * maxSpeed, rigidbody2D.velocity.y);
+
+		anim.SetFloat("Speed", Mathf.Abs(move));
+
+		if (move > 0 && !isFacingRight) {
+			Flip ();
+		} else if (move < 0 && isFacingRight) {
+			Flip ();
+		}
+	}
+
+	private void Flip () {
+		isFacingRight = !isFacingRight;
+		Vector3 scale = transform.localScale;
+		scale.x *= -1;
+		transform.localScale = scale;
 	}
 
 }
