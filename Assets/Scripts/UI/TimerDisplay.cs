9228b04ed073e953033605f889972d924da0c3e2
Luiz Branco <me@luizbranco.com>
2015-01-11 01:52:06 -0500

    Add timer countdown

--- Assets/Scripts/UI/TimerDisplay.cs
+++ Assets/Scripts/UI/TimerDisplay.cs
@@ -1,15 +1,26 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class TimerDisplay : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	private int start = 5000;
+	private float counter;
+	public GameObject gameObj;
+	private Text txt;
+
+	private void Start() {
+		counter = Time.time;
+		txt = GetComponent<Text>();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
+		if ((Time.time - counter) > 1f) {
+			start -= 100;
+			txt.text = start.ToString();
+			counter = Time.time;
+		}
+
 	}
 }
