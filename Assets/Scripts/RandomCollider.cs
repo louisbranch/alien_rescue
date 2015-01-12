0cda16634644000b688e265a31a994a07b904250
Luiz Branco <me@luizbranco.com>
2015-01-12 09:04:17 -0500

    Add rnd collision for spinners

---
+++ Assets/Scripts/RandomCollider.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomCollider : MonoBehaviour {
+
+	public float collisionSkipRate = 0.3f;
+
+	private void OnCollisionEnter2D(Collision2D coll) {
+		string name = coll.gameObject.name;
+		if (name == "Spinner") {
+			if (Random.value < collisionSkipRate) {
+				Physics2D.IgnoreCollision(collider2D, coll.collider, true);
+			}
+		}
+	}
+}
