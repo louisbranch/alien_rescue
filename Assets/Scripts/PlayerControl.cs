f1c09afcad4557ab1d47553b460f7ae4e317f8d8
Luiz Branco <me@luizbranco.com>
2015-01-11 19:46:00 -0500

    Add initial ladder movement

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -10,6 +10,8 @@ public class PlayerControl : MonoBehaviour {
 	private bool facingRight = true;
 	private bool grounded = false;
   	private bool jump = false;
+	private bool canClimb = false;
+	private bool climbing = false;
 
 	public Transform movementChecker;
 	public float checkerRadius = 0.1f;
@@ -23,12 +25,23 @@ public class PlayerControl : MonoBehaviour {
 
 	private void Update () {
 
+		float vMove = Input.GetAxis("Vertical");
+
 		grounded = Physics2D.OverlapCircle(movementChecker.position, checkerRadius, groundLayer);
 
 		anim.SetBool("Grounded", grounded);
 
-		if (Input.GetButtonDown("Jump") && grounded) {
+		if (Input.GetButtonDown("Jump") && grounded && !climbing) {
 			jump = true;
+		}
+		
+		if (canClimb && vMove != 0 && (grounded || climbing)) {
+			StartClimbing();
+			if (vMove > 0) {
+				transform.Translate (Vector2.up * vSpeed * Time.deltaTime);
+			} else {
+				transform.Translate (-Vector2.up * vSpeed * Time.deltaTime);
+			}
 		}
 
 	}
@@ -36,8 +49,7 @@ public class PlayerControl : MonoBehaviour {
 	private void FixedUpdate () {
 
 		float hMove = Input.GetAxis("Horizontal");
-		//float vMove = Input.GetAxis("Vertical");
-
+	
 		if (grounded) {
 			rigidbody2D.velocity = new Vector2(hMove * hSpeed, rigidbody2D.velocity.y);
 			anim.SetFloat("HSpeed", Mathf.Abs(hMove));
@@ -45,9 +57,11 @@ public class PlayerControl : MonoBehaviour {
 			if (hMove > 0 && !facingRight) {
 				Transform2D.FlipX(gameObject);
 				facingRight = !facingRight;
+				DisableClimbing();
 			} else if (hMove < 0 && facingRight) {
 				Transform2D.FlipX(gameObject);
 				facingRight = !facingRight;
+				DisableClimbing();
 			}
 		}
 		
@@ -66,12 +80,40 @@ public class PlayerControl : MonoBehaviour {
 		}
 	}
 
+	private void OnTriggerEnter2D(Collider2D coll) {
+		string name = coll.gameObject.name;
+		if (name == "Ladder") {
+			canClimb = true;
+		}
+	}
+	
+	private void OnTriggerExit2D(Collider2D coll) {
+		string name = coll.gameObject.name;
+		if (name == "Ladder") {
+			DisableClimbing();
+		}
+	}
+
   	//TODO move to other script
 	private void KillPlayer () {
 		anim.SetTrigger("Dead");
 		rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, 5f);
 		collider2D.enabled = false;
 		GameControl.LifeLost();
+	}
+
+	private void StartClimbing() {
+		climbing = true;
+		rigidbody2D.gravityScale = 0;
+		rigidbody2D.velocity = new Vector2(0, 0);
+		anim.SetBool("Climbing", true);
+	}
+
+	private void DisableClimbing() {
+		climbing = false;
+		canClimb = false;
+		rigidbody2D.gravityScale = 1;
+		anim.SetBool("Climbing", false);
 	}
 
 }
