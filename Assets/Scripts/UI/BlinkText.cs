1c2bfdc9bf1a422acfdf996eab544660cb2ccb7e
Luiz Branco <me@luizbranco.com>
2015-01-12 08:28:31 -0500

    Add help text for Princess

---
+++ Assets/Scripts/UI/BlinkText.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class BlinkText : MonoBehaviour {
+
+	public float seconds = 2f;
+
+	public string[] dialogs;
+
+	private Text guiText;
+
+	private void Awake () {
+		guiText = GetComponent<Text>();
+	}
+
+	private void Start () {
+		InvokeRepeating("ToggleText", seconds, seconds);
+	}
+
+	private void ToggleText () {
+		int i = Random.Range(0, dialogs.Length);
+		string dialog = dialogs[i];
+		guiText.text = dialog;
+		guiText.enabled = !guiText.enabled;
+	}
+}
